234d112f26fc14e1a24159769639ac403775974b
MaxDrg <maximadda2001@gmail.com>
2022-11-08 05:28:38 +0100

    user photo

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
+from django import forms
  
 class SignUpForm(UserCreationForm):
+    image = forms.ImageField(required=False)
+
     class Meta:
         model = CustomUser
-        fields = ('first_name', 'last_name', 'email')
+        fields = ('first_name', 'last_name', 'email', 'image')
         
     def __init__(self, *args, **kwargs):
         super(UserCreationForm, self).__init__(*args, **kwargs)
--- accounts/models.py
+++ accounts/models.py
@@ -2,9 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    username = None
-    email = models.EmailField(('E-mail address'), unique=True)
+    username    = None
+    email       = models.EmailField(('E-mail address'), unique=True)
+    image       = models.ImageField("User photo", null=True)
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
     def __str__(self):
-        return self.username
+        return self.email
