40f5f9441229bfddaa81492dca93ff142b50b655
MaxDrg <maximadda2001@gmail.com>
2022-11-09 10:28:32 +0100

    add message, reply models

--- forum/admin.py
+++ forum/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Topic
+from .models import Topic, Message, Reply
 
 admin.site.register(Topic)
+admin.site.register(Message)
+admin.site.register(Reply)
